6e9d2bbb9fb0e74636c3b24a7e183e9ba09169c2
AnsElgyar <anselgyar27@gmail.com>
2026-08-29 16:31:36 +0300

    Adding the logo to the App

--- backend/obj/Debug/net8.0/MarketIQ.Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/MarketIQ.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketIQ.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd1c331e1799c3881845192d9d8b35f18d79843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edbf4d19aed7c09d056e5a6da8026506716fc122")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketIQ.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketIQ.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
